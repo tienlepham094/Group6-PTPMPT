d1d080c23262f91fe14dfbab9b941770d451dd12
Duy Thai <you@example.com>
2024-10-23 10:36:17 +0700

    create project structur

---
+++ resoucesmanager/src/main/java/ltu/group06/work/resoucesmanager/ResoucesmanagerApplication.java
@@ -0,0 +1,13 @@
+package ltu.group06.work.resoucesmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResoucesmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResoucesmanagerApplication.class, args);
+	}
+
+}
---
+++ resoucesmanager/src/test/java/ltu/group06/work/resoucesmanager/ResoucesmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package ltu.group06.work.resoucesmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResoucesmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
