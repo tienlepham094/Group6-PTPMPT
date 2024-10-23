88bd848abe457d775f75231c5989d106544e2039
pelongngoan <long.tl446@gmail.com>
2024-10-23 16:55:24 +0700

    fe/feature-dev/inital project structure and simple routing

---
+++ frontend/src/views/Login.tsx
@@ -0,0 +1,98 @@
+import { LockOutlined } from "@mui/icons-material";
+import {
+  Container,
+  CssBaseline,
+  Box,
+  Avatar,
+  Typography,
+  TextField,
+  Button,
+  Grid,
+} from "@mui/material";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom"; // Import useNavigate
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate(); // Khởi tạo useNavigate
+
+  const handleLogin = () => {
+    // Ví dụ kiểm tra đăng nhập đơn giản
+    if (email === "test@example.com" && password === "password") {
+      // Điều hướng về trang chủ khi đăng nhập thành công
+      navigate("/home");
+    } else {
+      // Xử lý lỗi đăng nhập nếu cần
+      alert("Login failed!");
+    }
+  };
+
+  return (
+    <>
+      <Container maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            mt: 20,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: "primary.light" }}>
+            <LockOutlined />
+          </Avatar>
+          <Typography variant="h5">Login</Typography>
+          <Box>
+            <Typography>Email: test@example.com </Typography>
+            <Typography>Password: password </Typography>
+          </Box>
+          <Box sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoFocus
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="password"
+              name="password"
+              label="Password"
+              type="password"
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+            />
+
+            <Button
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+              onClick={handleLogin}
+            >
+              Login
+            </Button>
+            <Grid container justifyContent={"flex-end"}>
+              <Grid item>
+                <Link to="/register">Don't have an account? Register</Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
+    </>
+  );
+};
+
+export default Login;
