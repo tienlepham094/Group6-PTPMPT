6449724ab30eefc966a7b0f74deee4030ea839fd
Duy Thai <you@example.com>
2024-10-23 11:48:22 +0700

    test controller

--- backend/src/main/java/ltu/group06/work/resoucesmanager/BackendApplication.java
+++ backend/src/main/java/ltu/group06/work/resoucesmanager/BackendApplication.java
@@ -2,12 +2,11 @@ package ltu.group06.work.resoucesmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 public class BackendApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
     }
-
 }
