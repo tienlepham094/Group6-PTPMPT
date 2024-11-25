7d60e8ac90c462b1909fe0401ff04138c7fb5b9d
pelongngoan <long.tl446@gmail.com>
2024-11-25 10:18:59 +0700

    [FE][Feature]Add and update model type and enum

--- frontend/src/api/types.ts
+++ frontend/src/api/types.ts
@@ -1,4 +1,5 @@
 import {
+  APPROVALSTATUS,
   REQUESTSTATUS,
   RESOURCETYPE,
   ROLE,
@@ -8,35 +9,74 @@ import {
 
 type Account = {
   id: string;
-  firstName: string;
-  lastName: string;
-  displayName: string;
-  birthday: number;
-  phone: string;
   email: string;
   role: ROLE;
   password: string;
-  token: string | null;
   state: STATE | string;
   option: string | null;
   createdAt: number;
   updatedAt: number;
 };
-type Profile = {};
+type User = {
+  userId: number;
+  username: string;
+  email: string;
+  passwordHash: string;
+  role: ROLE;
+  isActive: boolean;
+  otps: OTP;
+  requests: Request;
+  approvals: Approval;
+  createdAt: string;
+  updatedAt: string;
+};
+type OTP = {
+  id: number;
+  otpCode: string;
+  createdAt: string;
+  expiresAt: string;
+  user: User;
+  expired: string;
+};
 type Request = {
-  requestId: string;
-  user: USER;
+  requestId: number;
+  user: User;
   resourceType: RESOURCETYPE;
   quantity: number;
   startTime: string;
   end_time: string;
-  statusRequest: STATUSREQUEST; // pending, approved, rejected
+  statusRequest: STATUSREQUEST;
   reason: string;
-  timeUsage: string; // in hours
-  approval: string;
-  allocations: string;
+  timeUsage: number;
+  approval: Approval;
+  allocations: Allocation;
   createdAt: string;
   updatedAt: string;
   equestStatus: REQUESTSTATUS;
 };
-export type { Account, Profile, Request };
+type Approval = {
+  approvalId: number;
+  request: Request;
+  admin: User;
+  approvalStatus: APPROVALSTATUS;
+  comments: string;
+  approvedAt: string;
+};
+type Allocation = {
+  allocationId: number;
+  request: Request;
+  resource: Resource;
+  allocatedQuantity: number;
+  allocatedAt: string;
+  releasedAt: string;
+};
+type Resource = {
+  resourceId: number;
+  resourceType: RESOURCETYPE;
+  quantity: string;
+  statusResources: REQUESTSTATUS;
+  allocations: Allocation;
+  createdAt: string;
+  updatedAt: string;
+};
+export type { Account, Request, Allocation, Approval, OTP, Resource, User };
